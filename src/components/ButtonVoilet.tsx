42b88a99f53e72861d9902a4529849cf6e741b2f
Kanishk-mittal <kanishkmittal2005@gmail.com>
2025-09-03 08:33:58 +0530

    Fix button margin in ButtonGreen and ButtonVoilet components

--- src/components/ButtonVoilet.tsx
+++ src/components/ButtonVoilet.tsx
@@ -3,7 +3,7 @@ import type { ButtonProps } from "../types/Button"
 const ButtonVoilet = (input: ButtonProps) => {
     return (
         <button
-            className="bg-[#7c3bec] active:bg-violet-700 rounded-xl text-white py-2 px-4 mx-2 cursor-pointer"
+            className="bg-[#7c3bec] active:bg-violet-700 rounded-xl text-white py-2 px-4 m-2 cursor-pointer"
             onClick={input.onClick}
             onMouseEnter={input.onHover}
         >
